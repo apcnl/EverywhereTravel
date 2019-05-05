975138d8ec90a2ebfc50acb75958302d6093e06a
Your Name <you@example.com>
2019-05-05 14:24:19 +0800

    baseapp

--- app/src/main/java/com/example/apcnl/travel/base/BaseApp.java
+++ app/src/main/java/com/example/apcnl/travel/base/BaseApp.java
@@ -1,6 +1,10 @@
 package com.example.apcnl.travel.base;
 
 import android.app.Application;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
@@ -10,6 +14,10 @@ import com.umeng.socialize.PlatformConfig;
  */
 
 public class BaseApp extends Application{
+
+    private static int mWidthPixels;
+    private static int mHeightPixels;
+    private static BaseApp sBaseApp;
 
     @Override
     public void onCreate() {
@@ -21,5 +29,23 @@ public class BaseApp extends Application{
         //豆瓣RENREN平台目前只能在服务器端配置
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+    }
+
+    //计算屏幕宽高
+    private void getScreenWH() {
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display defaultDisplay = manager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        defaultDisplay.getMetrics(metrics);
+        mWidthPixels = metrics.widthPixels;
+        mHeightPixels = metrics.heightPixels;
+    }
+
+    public static BaseApp getInstance(){
+        return sBaseApp;
+    }
+
+    public static Resources getRes() {
+        return sBaseApp.getResources();
     }
 }
